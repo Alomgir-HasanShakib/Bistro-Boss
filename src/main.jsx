5037f8b1a4d294ebe0edf9cc1babed126a122cef
Shakib <alomgirhasanshakib@gmail.com>
2024-05-31 18:29:12 +0600

    added tanstack query

--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,21 @@ import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import router from "./Routers/Router";
 import Authentication from "./Context/Authentication";
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Authentication>
+   <QueryClientProvider client={queryClient}>
+   <Authentication>
       <div className="container mx-auto">
         <RouterProvider router={router}></RouterProvider>
       </div>
     </Authentication>
+   </QueryClientProvider>
   </React.StrictMode>
 );
